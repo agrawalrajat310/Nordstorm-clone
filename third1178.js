797cba06fb63232aac1652bbcd0b14a5d5b471db
tarun-upadhyay <tarunu88@gmail.com>
2022-08-27 04:12:37 +0530

    day-3

--- third1178.js
+++ third1178.js
@@ -1,45 +1,132 @@
-import { footer,header } from "./header_footer/footer_header.js";
+import { footer, header } from "./header_footer/footer_header.js";
 
 document.getElementById("header1").innerHTML = header();
 document.getElementById("footer1").innerHTML = footer();
 
+// var options = {
+//   offset: { vertical: 0, horizontal: 20 },
+//   scale: 0.5,
+// };
 
+//new ImageZoom(document.getElementById("child_image"), options);
 
-
-const dropdowns = document.querySelectorAll(".dropdown");
-
-dropdowns.forEach((dropdown)=>{
-    //get inner element from each dropdown
-    const  select = dropdown.querySelector(".select");
-const caret = dropdown.querySelector(".caret");
-const menu = dropdown.querySelector(".menu");
-const options = dropdown.querySelector(".menu li");
-const selected = dropdown.querySelector(".selected");
-//we are using this method in order to have
-//multiple dropdown menus on the page work
-select.addEventListener("click", ()=>{
-    //add the clicked select styles to the select element
-    select.classList.toggle("select-clicked");
-    //add the rotate styles to the caret element;
-    caret.classList.toggle('caret-rotate');
-    //add the open styles to the menu element;
-    menu.classList.toggle("menu-open")
-
+let addToCart = document.getElementById("add_to_bag");
+addToCart.addEventListener("click", ()=>{
+    cartadd()
 });
-//loop through all option elements
-options.forEach(option => {
-    option.addEventListener("click", ()=>{
-        selected.innerText = option.innerText;
-        select.classList.remove("select-clicked");
-        caret.classList.remove("caret-rotate");
-        menu.classList.remove('menu-open');
 
-        options.forEach(option =>{
-            option.classList.remove("active");
-        });
-        option.classList.add("active");
-    });
-});
-})
+
+
+let secondpage = [ {
+    image:
+      "https://n.nordstrommedia.com/id/sr3/f9091c49-39c7-4610-85d8-3dadf5b78885.jpeg?h=365&w=240&dpr=2",
+       title: "Pampa Travel Lite Boot",
+    price: "8420",
+    des: "PALLADIUM",
+    detail:
+      "A bungee lace system grants a comfortable but flexible fit to this adventure-ready boot that is detailed with bright neon accents on the tongue and pull-tabs.",
+  }];
+
+
+
+    let nord_img = document.getElementById("nord_img");
+   nord_img.src = secondpage[0].image;
+ 
+   let title_html = document.getElementById("title");
+   title_html.innerText = secondpage[0].title;
+
+   let price_html = document.getElementById("price");
+   price_html.innerHTML = `INR ${secondpage[0].price}`
+
+   let detail = document.getElementById("detail");
+   detail.innerText = secondpage[0].detail;
+
+   let des = document.getElementById("des");
+   des.innerText = secondpage[0].des
+
+
+
+
+function cartadd(){
+    let obj = {
+        img : secondpage[0].image,
+        title: secondpage[0].title,
+        price : secondpage[0].price,
+    }
+    
+    showSuccess(obj)
+  }
+
+function showSuccess(cart_arr){
+    /* shows success div and some cart info */
+  
+    //update cart img
+    const cartImg = document.getElementById("cartImg");
+    cartImg.setAttribute("src",secondpage[0].image);
+  
+    //update name and brand
+    const cartProductName = document.getElementById("cartProdName");
+    cartProductName.innerHTML = secondpage[0].title;
+
+    const cartProductBrand = document.getElementById("cartProdBrand");
+    cartProductBrand.innerHTML = secondpage[0].des;
+  
+    //update price
+    const cartPrice = document.getElementById("cartPrice");
+    cartPrice.innerHTML = secondpage[0].price;
+  
+    // //update count
+    // //get meta cart
+    // // let cart = JSON.parse(localStorage.getItem("metaCart"));
+    // // const cartCount = document.getElementById("cartCount");
+    // // cartCount.innerHTML = cart.count;
+    // // if(qty == 1){
+    // //   const cartQty = document.getElementById("cartQty");
+    // //   cartQty.innerHTML = ` (qty:${cartItem.qty})`;   
+    // // }else{
+    //     const cartQty = document.getElementById("cartQty");
+    //    cartQty.innerHTML = ` (qty:${cartItem.qty})`;
+    // }
+  
+    //finally show success div 
+    const successDiv = document.getElementById("currCart");
+    successDiv.style.visibility = "visible";
+  
+    return true;
+  }
+
+  document.getElementById("closeIcon").addEventListener("click", closeing)
+/* hide div after user clicks on close */
+function closeing(){
+    const successDiv = document.getElementById("currCart");
+    successDiv.style.visibility = "hidden";
+}
 
   
+  document.getElementById("checkoutBtn").addEventListener("click", checkout)
+
+
+  let cart_arr = JSON.parse(localStorage.getItem("cart")) || [];
+
+
+  function checkout(){
+    document.getElementById("add_to_bag").innerText = "In Your Bag"
+    let logged = JSON.parse(localStorage.getItem("isLogged"))
+
+    if(logged){
+        let obj = {
+            img : secondpage[0].image,
+            title: secondpage[0].title,
+            price : secondpage[0].price,
+        }
+        cart_arr.push(obj);
+   localStorage.setItem(("cart"), JSON.stringify(cart_arr));
+
+    }else{
+        alert("please login")
+        window.location.href = "./signIn.html"
+    }
+    closeing()
+}
+ 
+ 
