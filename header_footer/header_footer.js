6b271e7a464115b041c3166e2b43eb10bb44f10f
RAJAT AGRAWAL <agrawalrajat03@outlook.com>
2022-08-28 01:33:24 +0530

    delete

--- header_footer/header_footer.js
+++
@@ -1,4 +0,0 @@
-import { footer, header,head1 } from "./footer_header.js";
-document.getElementById("footer1").innerHTML = footer();
-document.getElementById("header1").innerHTML = header();
-document.querySelector(".class").innerHTML = head1();
